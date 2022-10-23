b703e899b4a5f22d2209f29ca35739711e374887
esolo <solodovnyk@gmail.com>
2022-10-23 12:31:59 +0300

    Create web.php

---
+++ laravel/code/web.php
@@ -0,0 +1,24 @@
+Route::controller(\App\Http\Controllers\PostController::class)
+    ->as('posts.')
+    ->prefix('posts')
+    ->group(function (){;
+            Route::match(['get'],'/', 'index')->name('index');
+            Route::get('/create', 'create')->name('create');
+            Route::post('/', 'store')->name('store');
+            Route::get('/{post}', 'show')->name('show');
+            Route::match(['put', 'patch'],'/{post}', 'update')->name('update');
+            Route::delete('/{post}', 'destroy')->name('destroy');
+            Route::get('/{post}/edit', 'edit')->name('edit');
+    });
+
+Route::group(['prefix'=>'posts', 'as'=>'posts.'], function (){
+    Route::get('/', [\App\Http\Controllers\PostController::class, 'index'])->name('index');
+    Route::post('/', [\App\Http\Controllers\PostController::class, 'store'])->name('store');
+    Route::get('/create', [\App\Http\Controllers\PostController::class, 'create'])->name('create');
+    Route::get('/{post}', [\App\Http\Controllers\PostController::class, 'show'])->name('show');
+    Route::match(['put', 'patch'], '/{post}', [\App\Http\Controllers\PostController::class, 'update'])->name('update');
+    Route::delete('/{post}', [\App\Http\Controllers\PostController::class, 'destroy'])->name('destroy');
+    Route::get('/{post}/edit', [\App\Http\Controllers\PostController::class, 'edit'])->name('edit');
+});
+
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
